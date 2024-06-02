be497347b63605c83559cbe7288ab6357536b79f
Christina Quast <contact@christina-quast.de>
2024-06-02 18:37:37 +0200

    rust_minimal: Add String and collect example
    In order to be able to overwrite the clone and from_iter methods for
    Vector, I need to use a wrapping MyVec struct, because we cannot
    implement an interface that is not defined in our rs file.
    
    Signed-off-by: Christina Quast <contact@christina-quast.de>

--- samples/rust/rust_minimal.rs
+++ samples/rust/rust_minimal.rs
@@ -3,7 +3,57 @@
 //! Rust minimal sample.
 
 use kernel::prelude::*;
+use alloc::string::String;
+use kernel::{str::CString, fmt};
+use core::ops::Deref;
 
+#[macro_export]
+macro_rules! vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                let _ = temp_vec.push($x, GFP_KERNEL);
+            )*
+            temp_vec
+        }
+    };
+}
+
+#[derive(Debug)]
+struct MyVec<T> (Vec<T>);
+
+impl<T> Deref for MyVec<T> {
+    type Target = Vec<T>;
+    fn deref(&self) -> &Vec<T> {
+        &self.0
+    }
+}
+
+impl<T:Clone> Clone for MyVec<T> {
+    fn clone(&self) -> Self {
+        let mut vec = Vec::new();
+        vec.try_reserve(self.len()).unwrap();
+        for elem in self.iter() {
+            vec.push(elem.clone(), GFP_KERNEL);
+        }
+        MyVec(vec)
+    }
+}
+
+
+impl<T> FromIterator<T> for MyVec<T> {
+    #[inline]
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> MyVec<T> {
+        let mut vec = Vec::new();
+        for i in iter {
+            let _ = vec.push(i, GFP_KERNEL);
+        }
+        MyVec(vec)
+    }
+}
+
+// TODO: marker trait exact_size_hint
 module! {
     type: RustMinimal,
     name: "rust_minimal",
@@ -21,10 +71,14 @@ impl kernel::Module for RustMinimal {
         pr_info!("Rust minimal sample (init)\n");
         pr_info!("Am I built-in? {}\n", !cfg!(MODULE));
 
-        let mut numbers = Vec::new();
-        numbers.push(72, GFP_KERNEL)?;
-        numbers.push(108, GFP_KERNEL)?;
-        numbers.push(200, GFP_KERNEL)?;
+        let numbers: Vec<i32> = vec![1, 2, 3];
+        pr_info!("numbers: {:?}\n", numbers);
+        let numbers_mapped:MyVec<i32> = numbers.iter().map(|x| x*x).collect();
+        pr_info!("numbers: {:?}\n", numbers_mapped.clone());
+
+        let s: &CStr = CStr::from_bytes_with_nul(b"Hello World!\0").unwrap();
+        let s2 = s.to_str().unwrap();
+        pr_info!("s: {:?}, ss: {:?}\n", s, s2.split("N").collect::<MyVec<&str>>());
 
         Ok(RustMinimal { numbers })
     }
